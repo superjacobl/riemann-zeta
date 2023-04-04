75aca9f1a82de12608c8c25ebb794f028d1cf887
Minh <propfeds@gmail.com>
2023-04-05 00:23:24 +0900

    First level of w1 is now free

--- theory.js
+++ theory.js
@@ -69,7 +69,8 @@ const bCost = new ExponentialCost(1e6, Math.log2(1e6));
 const getb = (level) => BigNumber.ONE + HALF * level;
 const getbTerm = (level) => BigNumber.TEN.pow(-getb(level));
 
-const w1Cost = new StepwiseCost(new ExponentialCost(150000, 3.6), 4);
+const w1Cost = new FirstFreeCost(new StepwiseCost(
+new ExponentialCost(150000, 3.6), 4));
 const getw1 = (level) => Utils.getStepwisePowerSum(level, 2, 8, 1);
 
 const w2Cost = new ExponentialCost(1e3, Math.log2(10));
