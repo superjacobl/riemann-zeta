66848d34715a10558cf9552d203dbb92272afcbf
Minh <propfeds@gmail.com>
2023-04-05 10:35:44 +0900

    Buffed w1, compensated w2;

--- theory.js
+++ theory.js
@@ -69,11 +69,10 @@ const bCost = new ExponentialCost(1e6, Math.log2(1e6));
 const getb = (level) => BigNumber.ONE + HALF * level;
 const getbTerm = (level) => BigNumber.TEN.pow(-getb(level));
 
-const w1Cost = new FirstFreeCost(new StepwiseCost(
-new ExponentialCost(150000, 3.6), 4));
+const w1Cost = new StepwiseCost(new ExponentialCost(150000, 3.6), 10);
 const getw1 = (level) => Utils.getStepwisePowerSum(level, 2, 8, 1);
 
-const w2Cost = new ExponentialCost(1e3, Math.log2(10));
+const w2Cost = new ExponentialCost(1e3, Math.log2(100));
 const getw2 = (level) => BigNumber.TWO.pow(level);
 
 const permaCosts =
